f18f8303bb9e8d4e3bbf7c48463cebd5947d9c1b
deepakkr14 <deepakbipin142@gmail.com>
2024-01-06 19:40:44 +0530

    adding cart modal using portals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from 'react';
 import Header from './Component/Layout/header';
 import Meals from './Component/Meals/Meals'
+import Cart from './Component/Cart/Cart';
 function App() {
   return (
     <Fragment>
+     <Cart/>
      <Header/>
      <main>
       <Meals/>
---
+++ src/Component/Cart/Cart.js
@@ -0,0 +1,25 @@
+import Modal from '../UI/Modal'
+import classes from './Cart.module.css'
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "suchi", price: "20", amount: "2" }].map((item) => {
+        <li>{item.name}</li>;
+      })}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total amount</span>
+        <span>34.45</span>
+      </div>
+      <div className={classes.actions}>
+      <button className={classes["button--alt"]}>Close</button>
+      <button className={classes.button}>Order</button>
+    </div>
+    </Modal>
+  );
+};
+export default Cart;
